6ec7a742e111250c8c416d156225c146ea0ac64b
Joonas Puur <joonas.puur@voco.ee>
2025-09-08 06:55:34 -0400

    testimiseks

---
+++ praks3/tervitus.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Autor: [Joonas]
+# Kuupäev: 08.09.2025
+# Kirjeldus: Skript küsib kasutajalt ees- ja perenime ning tervitab teda.
+
+echo -n "Sisesta oma ees- ja perenimi: "
+read eesnimi perenimi
+
+echo "Sinu eesnimi on $eesnimi ja perenimi on $perenimi"
+echo "Tere tulemast, $eesnimi $perenimi!"
