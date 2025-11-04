67d8a66a0c4c929af522f3dc92982a092e10a452
Joonas Puur <joonas.puur@voco.ee>
2025-11-04 14:48:00 +0200

    skript skript

--- praks4/paaris.sh
+++ praks4/paaris.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # See skript küsib kasutajalt täisarvu ja kontrollib, kas see on paaris või paaritu.
+# Kui sisend ei ole täisarv, kuvatakse veateade.
 
 read -p "Sisesta suvaline täisarv: " arv
 
-# Tingimus
-if [ $((arv % 2)) -eq 0 ]; then
-    echo "Antud arv on paaris"
+# Kontrollime, kas sisend koosneb ainult numbritest (ja võib olla ka miinusmärgiga alguses)
+if [[ $arv =~ ^-?[0-9]+$ ]]; then
+    if [ $((arv % 2)) -eq 0 ]; then
+        echo "Antud arv on paaris"
+    else
+        echo "Antud arv on paaritu"
+    fi
 else
-    echo "Antud arv on paaritu"
+    echo "VIGA: Palun sisesta ainult täisarv!"
 fi
+
